05bbb6db7a7a104dff244bbeeaba2ab3352f7d32
Shashidhar <shaskhu@b0f1d8765c58.ant.amazon.com>
2022-07-11 10:07:15 -0700

    src folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
-import { Button, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Button, Card, Form } from 'react-bootstrap';
 import { DataStore } from 'aws-amplify';
+import { Todo } from "./models";
+import * as uuid from 'uuid';
 
-function Todo({ todo, index, markTodo, removeTodo }) {
+
+function TodoItem({ todo, index, markTodo, removeTodo }) {
   return (
     <div
-      className="todo"
-      
-    >
+      className="todo">
       <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
       <div>
         <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
@@ -23,24 +23,28 @@ function Todo({ todo, index, markTodo, removeTodo }) {
 
 
 function FormTodo({ addTodo }) {
-  const [value, setValue] = React.useState("");
+  const [value, setValue] = React.useState();
+
+
 
   const handleSubmit = e => {
     e.preventDefault();
     if (!value) return;
+    console.log("in submit; adding: " + value);
     addTodo(value);
     setValue("");
-    
   };
+
+
 
   return (
     <Form onSubmit={handleSubmit}> 
     <Form.Group>
       <Form.Label><b>Add Todo</b></Form.Label>
       <Form.Control type="text" className="input" value={value} onChange={e => 
-setValue(e.target.value)} placeholder="Add new item" />
+setValue(e.target.value)} placeholder="Add new item"/>
     </Form.Group>
-    <Button variant="primary mb-3" type="submit" onClick={DataStore.save(value)}>
+    <Button variant="primary mb-3" type="submit">
       Submit
     </Button>
   </Form>
@@ -49,32 +53,83 @@ setValue(e.target.value)} placeholder="Add new item" />
 
 function App() {
 
-  const [todos, setTodos] = React.useState([
-    {
-      text: "Work on sample app!",
-      isDone: false
+
+
+  const [todos, setTodos] = React.useState([]);
+
+
+  React.useEffect(() => {
+    console.log("in use effect");
+    getItems();
+    const subscription = DataStore.observe(Todo).subscribe(() =>
+    getItems());
+    return () => subscription.unsubscribe();
+  }, []);
+
+
+  async function getItems() {
+    const items = null;
+    console.log(" in get items");
+    try{
+      const items = await DataStore.query(Todo);
+    } catch(e){
+      console.log("query failed");
+    }    
+    if(!items) {
+      console.log("Datastore empty");
+      return;
     }
-  ]);  
+    console.log("querying: " + items[0].text);
+    setTodos(items);
+  }
+
+
+
+
+
+  async function saveDB(temp){
+    console.log("in save;" + temp.text + " with ID: " + temp.id);
+    try{
+      await DataStore.save(temp); // getting stuck here??
+    } 
+    catch(e){
+        console.log("save failed ");
+    }
+    console.log("After saving .. yay!");
+    getItems();
+  }
+
+  // async function delDB(){
+  //   await DataStore.delete(todos);
+  // }
+
 
   const addTodo = text => {
-    const newTodos = [...todos, { text }];
+    const temp = new Todo({
+      id: uuid.v4(),
+      text: text,
+      isDone: false
+    })
+    const newTodos = [temp, ...todos];
+    console.log("in add; adding: " + newTodos[0].text + " with ID: " + temp.id);
+    saveDB(temp);
     setTodos(newTodos);
-    DataStore.save(newTodos);
-  };
+    
+  }
 
   const markTodo = index => {
     const newTodos = [...todos];
     newTodos[index].isDone = true;
-    DataStore.save(newTodos);
     setTodos(newTodos);
   };
 
   const removeTodo = index => {
     const newTodos = [...todos];
-    const del = newTodos.splice(index, 1);
-    DataStore.delete(del);
+    newTodos.splice(index, 1);
+    // delDB();
     setTodos(newTodos);
   };
+
 
 
 
@@ -83,12 +138,12 @@ function App() {
     <div className="app">
       <div className="container"> 
         <h1 className="text-center mb-4">Todo List</h1>
-        <FormTodo addTodo={addTodo} />
+        <FormTodo addTodo={addTodo}/>
         <div>
           {todos.map((todo, index) => (
             <Card>
               <Card.Body>
-                <Todo  
+                <TodoItem  
                 key={index}
                 index={index}
                 todo={todo}
@@ -104,6 +159,10 @@ function App() {
 );
 
 }
+
+
+
+
 
 
 export default App;
---
+++ src/models/schema.js
@@ -0,0 +1,57 @@
+export const schema = {
+    "models": {
+        "Todo": {
+            "name": "Todo",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "text": {
+                    "name": "text",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "isDone": {
+                    "name": "isDone",
+                    "isArray": false,
+                    "type": "Boolean",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                },
+                "updatedAt": {
+                    "name": "updatedAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": [],
+                    "isReadOnly": true
+                }
+            },
+            "syncable": true,
+            "pluralName": "Todos",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "5222271886f4cdf4ea36583af33925eb"
+};
